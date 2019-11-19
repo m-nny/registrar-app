09f9c0228041824de9bda8e97268438f77780279
Resetand <machehin00@gmail.com>
2019-11-19 16:05:47 +0300

    commit for linter

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -13,8 +13,9 @@ export type NewCourse = Omit<Course, '_id'>
 
 export type CourseEventHandler = (course: Course) => void;
 
-export function newCourse(abberiation: string, title: string, instructor: string, capacity: number, enrolled = 0): NewCourse {
+export function newCourse(abberiation: string, title: string, instructor: string, capacity: number, enrolled = 0): Course {
   return {
+    _id: abberiation,
     abberiation,
     title,
     instructor,
