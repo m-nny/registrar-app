cf63db6078dcae56f31d8e4df6dd21ed340e163f
Resetand <machehin00@gmail.com>
2019-11-20 18:50:56 +0300

    add airbnb rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withCSS = require('@zeit/next-css');
+
 module.exports = withCSS({
     /* config options here */
 });
--- src/components/CourseList/CourseList.tsx
+++ src/components/CourseList/CourseList.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Table, Button } from 'antd';
 import { ColumnProps } from 'antd/es/table';
 
