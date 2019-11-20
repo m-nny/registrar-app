60a362d81e11bc561ba1068f28d130498d281266
Resetand <machehin00@gmail.com>
2019-11-20 15:58:46 +0300

    set up eslint settigs and inlcude prettier

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 // next.config.js
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
-  /* config options here */
-})
+    /* config options here */
+});
+
+module.exports = {
+    webpack(config, { dev }) {
+        if (dev) {
+            config.module.rules.push({
+                test: [/\.ts$/, /\.js$/, /\.tsx$/],
+                loader: 'eslint-loader',
+                exclude: ['/node_modules/', '/.next/', '/out/'],
+                enforce: 'pre',
+                options: {
+                    emitWarning: true,
+                },
+            });
+        }
+        return config;
+    },
+};
